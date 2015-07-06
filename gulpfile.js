d0b990ce9f6682fec2bd2fca7006f1d84f0471f3
woJburgess <jburgess@whiteoctober.co.uk>
2015-07-06 09:53:23 +0100

    gulp serve

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+
+var sass      = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename    = require('gulp-rename');
+var connect   = require('gulp-connect');
+
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+gulp.task('watch', function () {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('server', function() {
+  connect.server({
+    livereload: true,
+    port: 1337,
+    host: "localhost"
+  });
+});
+
+gulp.task('sass', function (done) {
+  gulp.src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('default', ['sass']);
+gulp.task('serve', ['sass', 'server', 'watch']);
