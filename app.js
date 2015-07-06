f6e9821a76229670d773cdd73fb16189b64b9ac4
woJburgess <jburgess@whiteoctober.co.uk>
2015-07-06 11:30:16 +0100

    start creating ui routes

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-angular.module('pattern-box', []);
+angular.module('pattern-box', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+      .state('home', {
+        url: "/home",
+        templateUrl: 'templates/home.html'
+      });
+  });
